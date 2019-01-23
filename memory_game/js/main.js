eadf25f409a5c91e1208b25bc8ba39d2571b62bd
Tom Hinton <tomhinton@Toms-MacBook-Pro.local>
2019-01-23 15:35:27 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -6,3 +6,16 @@ var cardFour = "king";
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
 
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0]
+cardsInPlay.push(cardOne)
+console.log("User flipped " + cardOne);
+var cardTwo = cards[1];
+cardsInPlay.push(cardTwo)
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1])
+alert("You found a match!");
+else
+	alert("Try Again");
